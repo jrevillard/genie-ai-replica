04c752b8644575458ac96e1685e07dd8cb0a78ae
david.forden <david.forden@itu.int>
2025-04-20 21:01:04 +0700

    Security enhancements to index.html

--- examples/gov-chat-backend/index.js
+++ examples/gov-chat-backend/index.js
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
     next();
 });
 

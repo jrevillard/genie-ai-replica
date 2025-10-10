e8abba816f23ad6ef0b952365d7ce67605ab00c8
david.forden <david.forden@itu.int>
2025-10-10 19:54:53 +0700

    Loosened CORS again

--- components/gov-chat-backend/index.js
+++ components/gov-chat-backend/index.js
@@ -475,7 +475,7 @@ const cspOptions = {
     styleSrc: ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com"],
     imgSrc: ["'self'", "data:"],
     fontSrc: ["'self'", "data:", "https://cdnjs.cloudflare.com"],
-    connectSrc: ["'self'", "wss://e2e-82-109.ssdcloudindia.net:8090", "http://localhost:*", "https://e2e-82-109.ssdcloudindia.net:*", "https://api.open-meteo.com", "https://ipapi.co", "https://nominatim.openstreetmap.org", "wss://genie-ai.itu.int:443", "ws://localhost:8090"],
+    connectSrc: ["'self'", "wss://e2e-82-109.ssdcloudindia.net:8090", "http://localhost:8090", "https://e2e-82-109.ssdcloudindia.net:*", "https://api.open-meteo.com", "https://ipapi.co", "https://nominatim.openstreetmap.org", "wss://genie-ai.itu.int:443", "ws://localhost:8090"],
     frameSrc: ["'none'"],
     objectSrc: ["'none'"],
     baseUri: ["'self'"],

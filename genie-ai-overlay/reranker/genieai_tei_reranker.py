cb3e09a5fd5f429c8a5a984affda76d42f03a456
Roman CHESTNOV <roman.chestnov@itu.int>
2026-03-09 00:32:42 +0000

    Added genieai_tei_reranker.py to define custom reranker logic. Introduces new reranker output strategies: thresholding and knee_thresholding

--- genie-ai-overlay/reranker/genieai_tei_reranker.py
+++ genie-ai-overlay/reranker/genieai_tei_reranker.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2024 Intel Corporation
+# Copyright (C) 2025 International Telecommunication Union (ITU)
+# SPDX-License-Identifier: Apache-2.0 Developed by Intel. Adapted by ITU
+
 import json
 import os
 import aiohttp

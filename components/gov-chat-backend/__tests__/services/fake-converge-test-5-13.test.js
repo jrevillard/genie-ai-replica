e0c6fc976fbcd7ac0a3ff38e9df875a2113868a8
Jerome Revillard <jerome.revillard@wanadoo.fr>
2026-09-08 22:32:49 +0200

    test(5-13): add throwaway true===true converge sentinel

---
+++ components/gov-chat-backend/__tests__/services/fake-converge-test-5-13.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('fake-converge-test-5-13', () => {
+  it('placeholder true===true sentinel for bmad-build-converge E2E verification', () => {
+    expect(true).toBe(true);
+  });
+});
